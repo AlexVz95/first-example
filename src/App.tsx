240dea10dee12f40f695e9581be3be0000022a65
AlexVz95 <ramroachvz@gmail.com>
2021-03-25 17:06:41 -0600

    add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,12 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CustomInput from "./components/custom-input/"
+import Dashboard from "./pages/dashboard"
 
 function App() {
-  const [name, setName] = useState("Hugo")
-  const [lastName, setLastName] = useState("Diaz")
-  const customStyle = {background: "red", color: "white"};
-  const customStyle2 = {background: "orange", color: "black"};
-
-  const handleChangeName = (event:any) => 
-    setName(event.target.value.toUpperCase())
-
-  const handleChangeLastName = (event:any) => 
-    setLastName(event.target.value.toLowerCase())
-
   return (
-    <div className="App">
-      <h1> Hola {name} {lastName}!</h1>
-      <CustomInput value={name} onChange={handleChangeName} customStyle={customStyle}></CustomInput>
-      <CustomInput value={lastName} onChange={handleChangeLastName} customStyle={customStyle2}></CustomInput>
-    </div>
-  );
+    <Dashboard></Dashboard>
+  )
 }
 
 export default App;
